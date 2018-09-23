d4e1a8cdcbc7b765a33e6edaf0385f8de65743bf
Shakil Ahmed <sasajib@gmail.com>
2018-09-23 17:02:45 +0600

    reverted

--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -74,17 +74,19 @@ while True:
 
             recongized = "output/video/a_" + str(index) + ".png"
             cv2.imwrite(recongized, frame)
+
             if writer is None and args["output"] is not None:
                 fourcc = cv2.VideoWriter_fourcc(*"MJPG")
                 writer = cv2.VideoWriter(args["output"], fourcc, 20, (frame.shape[1], frame.shape[0]), True)
-                
+
             if writer is not None:
                 writer.write(frame)
+            
 		
 		# update the list of names
         names.append(name)
 
-    # loop over the recognized faces
+    # # loop over the recognized faces
     # for ((top, right, bottom, left), name) in zip(boxes, names):
 	# 	# rescale the face coordinates
     #     top = int(top * r)
@@ -92,10 +94,10 @@ while True:
     #     bottom = int(bottom * r)
     #     left = int(left * r)
 
-	# 	# draw the predicted face name on the image
-    #     cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
-    #     y = top - 15 if top - 15 > 15 else top + 15
-    #     cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+	# 	# # draw the predicted face name on the image
+    #     # cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
+    #     # y = top - 15 if top - 15 > 15 else top + 15
+    #     # cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
     # # if the video writer is None *AND* we are supposed to write
 	# # the output video to disk initialize the writer
